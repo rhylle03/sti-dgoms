8db8e30a018effb9ce2ff9200556d5ba2ad47327
rhylle03 <rhyllejhonverzon@gmail.com>
2024-11-23 07:14:39 +0800

    update

--- src/components/auth/LoginButton.tsx
+++ src/components/auth/LoginButton.tsx
@@ -1,17 +1,18 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { loginAction } from "@/actions/users";
+import { supabase } from "@/utils/supabase/client";
+import { redirect } from "next/navigation";
 import { Provider } from "@supabase/supabase-js";
 import { useTransition } from "react";
 import { useRouter } from "next/navigation";
 
-export default function AzureLoginButton() {
+const LoginButton = () => {
   const router = useRouter();
-  const [isPending, startTransition] = useTransition();
+  const [isPending, startTranstion] = useTransition();
 
   const handleLogin = (provider: Provider) => {
-    startTransition(async () => {
+    startTranstion(async () => {
       const { errorMessage, url } = await loginAction(provider);
 
       if (!errorMessage && url) {
@@ -23,13 +24,14 @@ export default function AzureLoginButton() {
   };
 
   return (
-    <Button
-      variant="outline"
-      className="h-10 px-6 rounded-md border border-gray-200 bg-white text-sm font-medium text-gray-900 shadow-sm hover:bg-gray-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500 disabled:opacity-50 disabled:cursor-not-allowed"
-      onClick={() => handleLogin("azure")}
-      disabled={isPending}
-    >
-      {isPending ? "Logging in..." : "Login with Azure"}
-    </Button>
+    <>
+      <>
+        <button onClick={() => handleLogin("azure")} disabled={isPending}>
+          Login with Azure
+        </button>
+      </>
+    </>
   );
-}
+};
+
+export default LoginButton;
