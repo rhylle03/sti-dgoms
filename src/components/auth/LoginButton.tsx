0d600c894b2f5402f8bb2a1e3defbcfcb1cb4430
rhylle03 <rhyllejhonverzon@gmail.com>
2024-11-23 06:54:43 +0800

    update

--- src/components/auth/LoginButton.tsx
+++ src/components/auth/LoginButton.tsx
@@ -1,19 +1,17 @@
 "use client";
 
-import { loginAction } from "@/actions/users";
-import { supabase } from "@/utils/supabase/client";
-import { redirect } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { loginAction } from "@/actions/users";
 import { Provider } from "@supabase/supabase-js";
 import { useTransition } from "react";
 import { useRouter } from "next/navigation";
 
-const LoginButton = () => {
+export default function AzureLoginButton() {
   const router = useRouter();
-  const [isPending, startTranstion] = useTransition();
+  const [isPending, startTransition] = useTransition();
 
   const handleLogin = (provider: Provider) => {
-    startTranstion(async () => {
+    startTransition(async () => {
       const { errorMessage, url } = await loginAction(provider);
 
       if (!errorMessage && url) {
@@ -25,17 +23,13 @@ const LoginButton = () => {
   };
 
   return (
-    <>
-      <>
-        <Button
-          variant="outline"
-          className="h-10 px-6 rounded-md border border-gray-200 bg-white text-sm font-medium text-gray-900 shadow-sm hover:bg-gray-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
-        >
-          Login with Azure
-        </Button>
-      </>
-    </>
+    <Button
+      variant="outline"
+      className="h-10 px-6 rounded-md border border-gray-200 bg-white text-sm font-medium text-gray-900 shadow-sm hover:bg-gray-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500 disabled:opacity-50 disabled:cursor-not-allowed"
+      onClick={() => handleLogin("azure")}
+      disabled={isPending}
+    >
+      {isPending ? "Logging in..." : "Login with Azure"}
+    </Button>
   );
-};
-
-export default LoginButton;
+}
