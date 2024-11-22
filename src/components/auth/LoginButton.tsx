12aaa0277fd53f9118a558698549425976f166eb
rhylle03 <rhyllejhonverzon@gmail.com>
2024-11-23 06:50:48 +0800

    update

--- src/components/auth/LoginButton.tsx
+++ src/components/auth/LoginButton.tsx
@@ -3,6 +3,7 @@
 import { loginAction } from "@/actions/users";
 import { supabase } from "@/utils/supabase/client";
 import { redirect } from "next/navigation";
+import { Button } from "@/components/ui/button";
 import { Provider } from "@supabase/supabase-js";
 import { useTransition } from "react";
 import { useRouter } from "next/navigation";
@@ -26,9 +27,12 @@ const LoginButton = () => {
   return (
     <>
       <>
-        <button onClick={() => handleLogin("azure")} disabled={isPending}>
+        <Button
+          variant="outline"
+          className="h-10 px-6 rounded-md border border-gray-200 bg-white text-sm font-medium text-gray-900 shadow-sm hover:bg-gray-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
+        >
           Login with Azure
-        </button>
+        </Button>
       </>
     </>
   );
